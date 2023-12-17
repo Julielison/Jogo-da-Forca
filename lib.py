8a24a7f317261a84f98b7d52461ace0bcc040f11
Julielison <127698034+Julielison@users.noreply.github.com>
2023-12-17 20:45:25 -0300

    mais comentários e pequenos ajustes

--- lib.py
+++ lib.py
@@ -1,10 +1,5 @@
 import random
 
-# Verifica se o arquivo está vazio ou não
-def arquivo_esta_vazio():
-    with open('dados.txt', 'r') as arquivo:
-        conteudo = arquivo.read()
-        return not conteudo
 
 # Menu jogar/sair e validar input
 def jogar():
@@ -16,30 +11,34 @@ def jogar():
         jogar()
     return True
 
+
 # Retorna o histórico de dados do jogador se o apelido for encontrado na base de dados, se não, retorna os dados zerados
 def verificar_apelido(apelido):
-    cont = 0
+    linha_jogador = 0
     with open('dados.txt', 'r', encoding='utf-8') as arquivo:
         for linha in arquivo:
             apelido_salvo, pontuação, palavras_adv = linha.split(';')
             if apelido_salvo == apelido:
-                return apelido, int(pontuação), palavras_adv.upper().rstrip('\n'), cont
-            cont += 1
+                return apelido, int(pontuação), palavras_adv.upper().rstrip('\n'), linha_jogador
+            linha_jogador += 1
         return apelido, int(0),'',-1
 
-# Carrega a palavra e a dica, se todas tiverem sido sortedas devolve None
+
+# Carrega a palavra e a dica. Se todas tiverem sido sorteadas, devolve None
 def carrega_palavra_dica(palavras_adv):
     palavras_sorteadas = set()
     with open('banco_de_palavras.txt', 'r', encoding='utf-8') as arquivo:
         linhas = arquivo.readlines()
-        
+
         while len(linhas) > len(palavras_sorteadas):
             palavra, dica = random.choice(linhas).strip().split(';')
             palavras_sorteadas.add(palavra)
             if palavra.upper() in palavras_adv:
                 continue
             return palavra.upper(), dica
+
     return None, None
+
 
 # Troca cada letra da palavra por um *
 def esconde_letras(palavra):
@@ -47,6 +46,7 @@ def esconde_letras(palavra):
         if letra != '-':
             palavra = palavra.replace(letra, '*')
     return palavra
+
 
 # Verifica se o chute foi repetido ou é inválido
 def validar_chute(chute,chutes):
@@ -57,6 +57,7 @@ def validar_chute(chute,chutes):
         print('Letra repetida! Digite outra!')
         return True
     return False
+
 
 #Funçâo para marcar o chute correto
 def marcar_chute_correto(palavra, chute, palavra_secreta):
@@ -70,27 +71,40 @@ def marcar_chute_correto(palavra, chute, palavra_secreta):
         index += 1
     return palavra_secreta_at
 
+
 # Verifica se a palavra foi adivinhada
 def acertou(palavra_secreta):
     return '*' not in palavra_secreta
 
+
+# Verifica se o arquivo está vazio ou não
+def arquivo_esta_vazio():
+    with open('dados.txt', 'r') as arquivo:
+        conteudo = arquivo.read()
+        return not conteudo
+
+
 #Atualiza o arquivo de dados
 def atualiza_dados(apelido,pontuação,palavras_adv,linha_jogador):
     palavras_adv_se = palavras_adv.strip()
+
     if linha_jogador >= 0:
         with open('dados.txt', 'r+', encoding='utf-8') as arquivo:
             linhas = arquivo.readlines()
-            linhas[linha_jogador] = f'{apelido};{pontuação};{palavras_adv_se}'
-            if linha_jogador != len(linhas) - 1:
-                linhas[linha_jogador] += '\n'
+            linhas[linha_jogador] = f'{apelido};{pontuação};{palavras_adv_se}\n'
+            # remove o \n caso a linha  a ser alterada seja a última do arquivo
+            if linha_jogador == len(linhas) - 1:
+                linhas[linha_jogador] = linhas[linha_jogador].rstrip('\n')
             arquivo.seek(0)
             arquivo.writelines(linhas)
-    else:
+
+    else: # caso o jogador não tenha sido encontrado
         with open('dados.txt', 'a', encoding='utf-8') as arquivo:
             if arquivo_esta_vazio():
                 arquivo.write(f'{apelido};{pontuação};{palavras_adv_se}')
             else:
                 arquivo.write(f'\n{apelido};{pontuação};{palavras_adv_se}')
+
 
 # Apaga os dados do jogador caso ele tenha zerado o jogo
 def apaga_jogador(linha_jogador):
@@ -98,10 +112,12 @@ def apaga_jogador(linha_jogador):
         linhas = arquivo.readlines()
         arquivo.seek(0)
         if len(linhas) - 1 == linha_jogador:
+            # remove o \n da linha anterior à última
             linhas[linha_jogador-1] = linhas[linha_jogador-1].rstrip('\n')
         del linhas[linha_jogador]
-        arquivo.writelines(linhas)
-        arquivo.truncate()
+        arquivo.writelines(linhas) # sobrescreve as linhas
+        arquivo.truncate() # remove o que há depois da última linha escrita
+
 
 # Desenha o boneco na forca
 def desenhar_boneco(erros):
--- main.py
+++ main.py
@@ -1,41 +1,45 @@
-from lib import *
+from lib import * # importa do arquivo lib todas as funções
 
-while jogar():
-    while True:
+while jogar(): # Inicia ou sai do jogo
+
+    while True: # Valida o apelido
       apelido = input('Digite um apelido: ')
-      if ';' in apelido or '-' in apelido:
-          print('Apelido inválido! Não pode conter ; ou -')
+      if ';' in apelido or apelido == '':
+          print('Apelido inválido! O apelido está vazio ou com ;')
           continue
       break
 
-    # Retorna os dados do jogador
-    while True:
-        pontos = erros = 0 
+    while True: # Carrega os dados do jogador e da palavra
+        pontos = erros = 0
         apelido, pontuação, palavras_adv,linha_jogador = verificar_apelido(apelido)
         palavra, dica = carrega_palavra_dica(palavras_adv)
-        if palavra == None:
-            print('Você zerou o jogo!')
-            apaga_jogador(linha_jogador)
-            break
-        palavra_secreta = esconde_letras(palavra)
 
+        if palavra == None: # Verifica se o jogador zerou o jogo
+            print('Você zerou o jogo!')
+            print(f'Pontuação final: {pontuação}')
+            apaga_jogador(linha_jogador) # apaga os dados do jogador
+            break
+
+        # Retorna a palavra com *
+        palavra_secreta = esconde_letras(palavra)
         chutes = ''
+
+        # Roda até que os erros cheguem a 6 ou a palavra seja acertada
         while erros < 6:
             print(f'Dica: {dica}')
             print(palavra_secreta)
             chute = input('Qual a letra? ').upper()
-
             if validar_chute(chute,chutes):
                 continue
             chutes += chute
 
-            if chute in palavra:
+            if chute in palavra: # verifica se o usuário acertou o chute
                 palavra_secreta = marcar_chute_correto(palavra, chute, palavra_secreta)
                 pontos += 10
-                if acertou(palavra_secreta):
+                if acertou(palavra_secreta): # verifica se acertou a palavra
                     print('Parabéns, você acertou!!!')
                     print(f'A palavra era {palavra}')
-                    palavras_adv += f' {palavra}'
+                    palavras_adv += f' {palavra}' # atualiza as palavras adivinhadas
                     pontuação += pontos
                     atualiza_dados(apelido,pontuação,palavras_adv,linha_jogador)
                     break
@@ -44,7 +48,7 @@ while jogar():
                 print(f'Você errou {erros} de 6 tentativas')
                 desenhar_boneco(erros)
 
-        # Pergunta se o usuário quer seguir ou não e atualiza os dados
+        # Pergunta se o usuário quer seguir ou não
         segue = input('Deseja continuar? (s/n) ').lower()
         if segue == 's':
             continue
